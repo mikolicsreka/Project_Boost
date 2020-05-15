9d60eaa42ecf225892ff150a4c49b2e3590ab7a9
mikolicsreka <mskyubi@gmail.com>
2020-05-15 16:47:00 +0200

    no message

---
+++ Project_Boost/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [Range(0f, 1f)][SerializeField] float movementFactor;
+
+    [SerializeField] float period = 2f;
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+
+    Vector3 startingPos;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float miatt nem vizsgálhatjuk h period == 0, mert akár egy kicsivel is de eltér
+        //az epszilon pedig a legkisebb szám ami..
+        if(period <= Mathf.Epsilon) { return; }
+        //set movement factor
+        float cycles = Time.time / period; //grows contiunally from 0
+
+        const float tau = Mathf.PI * 2; //about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau); //[-1,+1]
+
+        movementFactor = rawSinWave / 2f + 0.5f; //[0,1]
+
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPos + offset;
+    }
+}
---
+++ Project_Boost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float rcsThrust = 200f; //változtatható unityban, de más scriptek nem érik el
+
+
+    [SerializeField] float mainThrust = 50f;
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem deathParticles;
+
+    [SerializeField] float levelLoadDelay = 2f;
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+    enum State { Alive, Dying, Transcending };
+    State state = State.Alive;
+
+    bool collisionsAreEnabled = true;
+
+    bool isTransitioning = false; //enum helyett
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isTransitioning)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
+        if(Debug.isDebugBuild)
+        {
+            RespondToDebugKeys();
+        }
+
+    }
+
+    private void RespondToDebugKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextScene();
+        }
+        else if(Input.GetKeyDown(KeyCode.C))
+        {
+            collisionsAreEnabled = !collisionsAreEnabled; // kapcsoló
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (isTransitioning || !collisionsAreEnabled)
+        {
+            return;
+        }
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            default:
+                StartDeathSequence();
+                break;
+        }
+    }
+
+    private void StartSuccessSequence()
+    {
+        isTransitioning = true;
+
+        print("Finish");
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        successParticles.Play(); //ha betölti az uj szintet leáll, nem kell .Stop()
+        Invoke("LoadNextScene", levelLoadDelay); //1 sec waittime
+    }
+
+    private void StartDeathSequence()
+    {
+        isTransitioning = true;
+
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        deathParticles.Play();
+        Invoke("LoadFirstLevel", levelLoadDelay);
+    }
+
+
+
+    private void LoadFirstLevel()
+    {
+
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    private void RespondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
+        {
+            mainEngineParticles.Play();
+            ApplyThrust();
+
+        }
+        else
+        {
+            StopApplyingThrust();
+        }
+    }
+
+    private void StopApplyingThrust()
+    {
+        audioSource.Stop();
+        mainEngineParticles.Stop();
+    }
+
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime); //relative to the rockets coordinates
+        mainEngineParticles.Play();
+
+        if (!audioSource.isPlaying) //nem játsza egymás után sokszor
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        
+    }
+
+    private void RespondToRotateInput()
+    {
+ 
+
+
+        //float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            RotateManually(rcsThrust * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            RotateManually(-rcsThrust * Time.deltaTime);
+        }
+
+        
+
+    }
+
+    private void RotateManually(float rotationThisFrame)
+    {
+        rigidBody.freezeRotation = true; //take manuel control of rotation
+        transform.Rotate(Vector3.forward * rotationThisFrame);
+        rigidBody.freezeRotation = false; //resume physics control of object
+    }
+}
